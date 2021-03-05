64573908e1f85ada7555ff529e007a310c425f1f
WojtekBoman <wojciechboman@gmail.com>
2021-03-05 18:52:46 +0100

    fit isCurrentDay function

--- app/components/WeatherDaysList.js
+++ app/components/WeatherDaysList.js
@@ -7,7 +7,7 @@ import { useDispatch } from "react-redux";
 const WeatherDaysList = (props) => {
   const dispatch = useDispatch();
   const update = (day) => dispatch(updateDay(day));
-  const isCurrentDay = (date, currentDay) => date !== currentDay;
+  const isCurrentDay = (date, currentDay) => date === currentDay;
 
   return (
     <View>
@@ -16,14 +16,14 @@ const WeatherDaysList = (props) => {
           key={i}
           bottomDivider
           onPress={() => {
-            if (isCurrentDay(l.date, props.currentDay)) update(l.date);
+            if (!isCurrentDay(l.date, props.currentDay)) update(l.date);
           }}
         >
           <ListItem.Content>
             <ListItem.Title>{l.day}</ListItem.Title>
             <ListItem.Subtitle>{l.date}</ListItem.Subtitle>
           </ListItem.Content>
-          {isCurrentDay(l.date, props.currentDay) && <ListItem.Chevron />}
+          {!isCurrentDay(l.date, props.currentDay) && <ListItem.Chevron />}
         </ListItem>
       ))}
     </View>
